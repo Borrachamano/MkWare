901db5969cac444b377050fcc3022cc352b9c05f
Dur-hy <76396111+Borrachamano@users.noreply.github.com>
2021-05-22 12:26:00 -0400

    Update mkware.py

--- mkware.py
+++ mkware.py
@@ -1,31 +1,32 @@
 import os
 import random
-os.system("clear")
-print("""
 
-███╗   ███╗██╗  ██╗██╗    ██╗ █████╗ ██████╗ ███████╗
-████╗ ████║██║ ██╔╝██║    ██║██╔══██╗██╔══██╗██╔════╝
-██╔████╔██║█████╔╝ ██║ █╗ ██║███████║██████╔╝█████╗  
-██║╚██╔╝██║██╔═██╗ ██║███╗██║██╔══██║██╔══██╗██╔══╝  
-██║ ╚═╝ ██║██║  ██╗╚███╔███╔╝██║  ██║██║  ██║███████╗
-╚═╝     ╚═╝╚═╝  ╚═╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
-                                                     
-Desenvolvido by >> Iboy
-Telegram >> @iboynerd
-Discord >> Iboy #3048
-Malware para fins educacionais.
-	""")
-
-decisao = str(input("Esse Malware irá criar varias pastas. S/N: "))
-if decisao=='s':
-	while True:
-	    ran = random.randint(10,55)
-	    os.system(f"mkdir {ran}")
-else:
+def main()
 	os.system("clear")
-	print("goodbye amigo")
-	exit()
+	print("""
 	
+	███╗   ███╗██╗  ██╗██╗    ██╗ █████╗ ██████╗ ███████╗
+	████╗ ████║██║ ██╔╝██║    ██║██╔══██╗██╔══██╗██╔════╝
+	██╔████╔██║█████╔╝ ██║ █╗ ██║███████║██████╔╝█████╗  
+	██║╚██╔╝██║██╔═██╗ ██║███╗██║██╔══██║██╔══██╗██╔══╝  
+	██║ ╚═╝ ██║██║  ██╗╚███╔███╔╝██║  ██║██║  ██║███████╗
+	╚═╝     ╚═╝╚═╝  ╚═╝ ╚══╝╚══╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝
+	                                                     
+	Desenvolvido by >> Iboy
+	Telegram >> @iboynerd
+	Discord >> Iboy #3048
+	Malware para fins educacionais.
+		""")
+	
+	decisao = str(input("Esse Malware irá criar varias pastas. S/N: "))
+	if decisao=='s':
+		while True:
+		    ran = random.randint(10,55)
+		    os.system(f"mkdir {ran}")
+	else:
+		os.system("clear")
+		print("goodbye amigo")
+		exit()
 
-
-
+if __name__ == '__main__':
+	main()
